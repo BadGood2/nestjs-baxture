57760343c919847592e7fc55c9597169f23272d2
mdnaseem <mdnaseem050998@gmail.com>
2024-03-14 13:29:04 +0530

    handled api exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppMultiModule } from './app.module.multi';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { LoadBalancerService } from './load-nodejs-cluster/load-nodejs-cluster.service';
 
 async function bootstrap() {
   if (process.env.NODE_ENV === 'cluster') {
     const app = await NestFactory.create(AppMultiModule);
+    app.useGlobalPipes(new ValidationPipe());
 
     const loadBalancerService = app.get(LoadBalancerService);
     await app.listen(loadBalancerService.getPort());
     Logger.log(`Application is running on: ${await app.getUrl()}`);
   } else {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.port || 3000);
   }
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,8 +9,10 @@ import {
   HttpCode,
   HttpStatus,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { UsersService } from './user.service';
+import { Types } from 'mongoose';
 import { CreateUserDto, UpdateUserDto } from './user.dto';
 
 @Controller('api/users')
@@ -24,6 +26,9 @@ export class UsersController {
 
   @Get(':id')
   async findById(@Param('id') id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Bad ObjectId in Body');
+    }
     const user = await this.usersService.findById(id);
     if (!user) {
       throw new NotFoundException('User not found');
@@ -39,6 +44,9 @@ export class UsersController {
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Bad ObjectId in Body');
+    }
     const updatedUser = await this.usersService.update(id, updateUserDto);
     if (!updatedUser) {
       throw new NotFoundException('User not found');
@@ -48,6 +56,9 @@ export class UsersController {
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException('Bad ObjectId in Body');
+    }
     const user = await this.usersService.delete(id);
     if (!user) {
       throw new NotFoundException('User not found');
